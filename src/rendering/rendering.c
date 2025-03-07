f6992188c781e9f81139251d357ce6af90f4614a
Imrane Halim <ihalim.professional@gmail.com>
2025-03-07 03:43:48 +0000

    added line drawing function

--- src/rendering/rendering.c
+++ src/rendering/rendering.c
@@ -8,18 +8,16 @@ void	put_pixel(t_coordinates coord, int color)
 
 	x = (int)coord.x;
 	y = (int)coord.y;
-
 	if (x < 0 || y < 0 || x >= WIDTH || y >= HEIGHT)
 		return ;
-	dst = g_game.window.frame.adr
-		+ (y * g_game.window.frame.line_length
-		+ x * (g_game.window.frame.bbp / 8));
-	*(unsigned int*)dst = color;
+	dst = g_game.window.frame.adr + (y * g_game.window.frame.line_length + x
+			* (g_game.window.frame.bbp / 8));
+	*(unsigned int *)dst = color;
 }
 
-//--------------------
+//-------------------------- some core stuff
 
-void	draw_square(t_coordinates coord, int height, int width ,int color)
+void	draw_square(t_coordinates coord, int height, int width, int color)
 {
 	int	save_x;
 	int	save_y;
@@ -38,9 +36,34 @@ void	draw_square(t_coordinates coord, int height, int width ,int color)
 	}
 }
 
-void	draw_map()
+void	draw_line(t_coordinates start, t_coordinates end, int color)
 {
-	char **map;
+	int				dx;
+	int				dy;
+	int				step;
+	t_coordinates	incr;
+	t_coordinates	cord;
+
+	dx = (int)end.x - (int)start.x;
+	dy = (int)end.y - (int)start.y;
+	step = abs(dy);
+	if (abs(dx) > abs(dy))
+		step = abs(dx);
+	incr.x = (float)dx / step;
+	incr.y = (float)dy / step;
+	cord.x = start.x;
+	cord.y = start.y;
+	for (int i = 0; i < step; i++)
+	{
+		put_pixel(cord, color);
+		cord.x += incr.x;
+		cord.y += incr.y;
+	}
+}
+// ------------------------- 2D helpers
+void	draw_map(void)
+{
+	char	**map;
 	int		x;
 	int		y;
 
@@ -58,30 +81,41 @@ void	draw_map()
 		}
 		y++;
 	}
-	
 }
 
-void	draw_player()
+void	draw_player(void)
 {
+	int	endx;
+	int	endy;
+	int	line_lenght;
+
+	line_lenght = 100;
+	// draw player square
 	draw_square(g_game.player.pos, 16, 16, 0xf2a200);
+	// draw player looking direction
+	endx = (int)g_game.player.pos.x + cos(g_game.player.angle) * line_lenght;
+	endy = (int)g_game.player.pos.y + sin(g_game.player.angle) * line_lenght;
+	draw_line(g_game.player.pos, (t_coordinates){endx, endy}, 0xff0000);
 }
 
 //--------------------------
 
-void	draw_2D_view()
+void	draw_2D_view(void)
 {
 	draw_square((t_coordinates){0, 0}, HEIGHT, WIDTH, 0x2e2d2d);
 	draw_map();
 	draw_player();
 }
-// void	draw_3D_view(){}
+// void	draw_3D_view(void)
+//{}
 
 //-----------------------
 
-int		render_frame()
+int	render_frame(void)
 {
 	draw_2D_view();
 	// draw_3D_view();
-	mlx_put_image_to_window(g_game.window.mlx, g_game.window.win, g_game.window.frame.img, 0, 0);
-	return 0;
+	mlx_put_image_to_window(g_game.window.mlx, g_game.window.win,
+		g_game.window.frame.img, 0, 0);
+	return (0);
 }
