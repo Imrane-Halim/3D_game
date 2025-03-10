544bd8c645dacdd4af490e81594b04667663b23c
Imrane Halim <ihalim.professional@gmail.com>
2025-03-10 21:02:32 +0000

    moved some comments to notes.txt

--- src/ray_casting/cast_ray.c
+++ src/ray_casting/cast_ray.c
@@ -12,61 +12,10 @@ char obj_hit(t_xy cord)
     return (g_game.scene.map[map_y][map_x]);
 }
 
-/*
-// to prototypes, nothing serious overhere....
-
-	t_xy cast_ray(float angle) {
-    float px = g_game.player.pos.x;
-    float py = g_game.player.pos.y;
-    float dir_x = cos(angle);
-    float dir_y = sin(angle);
-
-    // Vertical intersections
-    t_xy vert_hit = find_vertical_hit(px, py, dir_x, dir_y);
-    // Horizontal intersections
-    t_xy horiz_hit = find_horizontal_hit(px, py, dir_x, dir_y);
-
-    // Compare distances and return closest hit
-    if (distance(vert_hit) < distance(horiz_hit))
-        return vert_hit;
-    else
-        return horiz_hit;
+float	distance(t_xy a, t_xy b)
+{
+	return (sqrt((b.x - a.x) * (b.x - a.x) + (b.y - a.y) * (b.y - a.y)));
 }
-*/
-
-
-/*
-source: https://permadi.com/1996/05/ray-casting-tutorial-7/
---------->finding horizontal intersection: 
-
-1. find the coordinates of the first ever point, let's call it A
-	--> for y:
-	if (ray facing up)
-		A.y = ((int)player.y / TILESIZE) * TILESIZE - 1;
-	if (ray facing down)
-		A.y = ((int)player.y / TILESIZE) * TILESIZE + TILESIZE;
-	
-	some explanation:
-		we must decide whether A is part of the block above the grid line, 
-		that is why we substract -1 from A.y
-	
-	--> for x:
-	A.x = player.x + (player.y - A.y) / tan (RAY_ANGLE)
-
-2. find offsets
-	if (ray facing up)
-		Ya = -TILESIZE;
-	if (ray facing down)
-		Ya = TILESIZE;
-	
-	Xa = TILESIZE / tan(FOV)
-
-3. check the grid for a wall
-4. if there is no wall, move to next point:
-	Ynew = Yold + Ya;
-	Xnew = Xold + Xa;
-*/
-
 
 t_xy find_vertical_hit(t_xy p, float angle)
 {
@@ -128,11 +77,6 @@ t_xy find_horizontal_hit(t_xy p, float angle)
         A.y += Offset.y;
     }
     return A;
-}
-
-float	distance(t_xy a, t_xy b)
-{
-	return (sqrt((b.x - a.x) * (b.x - a.x) + (b.y - a.y) * (b.y - a.y)));
 }
 
 // note: angle is in radian
