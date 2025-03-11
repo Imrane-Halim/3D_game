b337db2dcd8141668d948bcacb6f651d55af01e9
Imrane Halim <ihalim.professional@gmail.com>
2025-03-11 00:58:03 +0000

    fixed some bugs in ray casting

--- src/ray_casting/cast_ray.c
+++ src/ray_casting/cast_ray.c
@@ -2,14 +2,14 @@
 
 char obj_hit(t_xy cord)
 {
-    int map_x = (int)(cord.x / TILESIZE);
-    int map_y = (int)(cord.y / TILESIZE);
-    
-    if (map_x < 0 || map_x >= g_game.scene.m_width)
-        return (0);
-    if (map_y < 0 || map_y >= g_game.scene.m_height)
-        return (0);
-    return (g_game.scene.map[map_y][map_x]);
+	int map_x = (int)(cord.x / TILESIZE);
+	int map_y = (int)(cord.y / TILESIZE);
+	
+	if (map_x < 0 || map_x >= g_game.scene.m_width)
+		return (0);
+	if (map_y < 0 || map_y >= g_game.scene.m_height)
+		return (0);
+	return (g_game.scene.map[map_y][map_x]);
 }
 
 float	distance(t_xy a, t_xy b)
@@ -19,75 +19,75 @@ float	distance(t_xy a, t_xy b)
 
 t_xy find_vertical_hit(t_xy p, float angle)
 {
-    t_xy A;
-    t_xy Offset;
-    
-    if (angle > PI/2 && angle < 3*PI/2)
+	t_xy	A;
+	t_xy	step;
+
+	if (cos(angle) > 0)
 	{
-        A.x = ((int)p.x / TILESIZE) * TILESIZE - 1;
-        Offset.x = -TILESIZE;
-    }
+		A.x = floor(p.x / TILESIZE) * TILESIZE + TILESIZE;
+		step.x = TILESIZE;
+	}
 	else
 	{
-        A.x = ((int)p.x / TILESIZE) * TILESIZE + TILESIZE;
-        Offset.x = TILESIZE;
-    }
-    A.y = p.y + (p.x - A.x) * tan(angle);
-    Offset.y = Offset.x * tan(angle);
-    while (true)
+		A.x = floor(p.x / TILESIZE) * TILESIZE - 0.00001;
+		step.x = -TILESIZE;
+	}
+	A.y = p.y + (A.x - p.x) * tan(angle);
+	step.y = step.x * tan(angle);
+	while (true)
 	{
-        if (A.x < 0 || A.x >= WIDTH || A.y < 0 || A.y >= HEIGHT)
-            break;
-            
-        if (obj_hit(A) == '1')
-            break;
-            
-        A.x += Offset.x;
-        A.y += Offset.y;
-    }
-    return A;
+		if (A.x < 0 || A.x >= WIDTH || A.y < 0 || A.y >= HEIGHT)
+			break;
+			
+		if (obj_hit(A) != '0')
+			break;
+			
+		A.x += step.x;
+		A.y += step.y;
+	}
+	return (A);
 }
 
 t_xy find_horizontal_hit(t_xy p, float angle)
 {
-    t_xy A;
-    t_xy Offset;
-    
-    if (angle > PI && angle < 2*PI)
+	t_xy	A;
+	t_xy	step;
+
+	if (sin(angle) > 0)
 	{
-        A.y = ((int)p.y / TILESIZE) * TILESIZE - 1;
-        Offset.y = -TILESIZE;
-    }
+		A.y = floor(p.y / TILESIZE) * TILESIZE + TILESIZE;
+		step.y = TILESIZE;
+	}
 	else
 	{
-        A.y = ((int)p.y / TILESIZE) * TILESIZE + TILESIZE;
-        Offset.y = TILESIZE;
-    }
-    A.x = p.x + (p.y - A.y) / tan(angle);
-    Offset.x = TILESIZE / tan(angle);
-    while (true)
+		A.y = floor(p.y / TILESIZE) * TILESIZE - 0.00001;
+		step.y = -TILESIZE;
+	}
+	A.x = p.x + (A.y - p.y) / tan(angle);
+	step.x = step.y / tan(angle);
+	while (true)
 	{
-        if (A.x < 0 || A.x >= WIDTH || A.y < 0 || A.y >= HEIGHT)
-            break;
-            
-        if (obj_hit(A) == '1')
-            break;
-            
-        A.x += Offset.x;
-        A.y += Offset.y;
-    }
-    return A;
+		if (A.x < 0 || A.x >= WIDTH || A.y < 0 || A.y >= HEIGHT)
+			break;
+			
+		if (obj_hit(A) != '0')
+			break;
+			
+		A.x += step.x;
+		A.y += step.y;
+	}
+	return (A);
 }
 
 // note: angle is in radian
 t_xy	cast_ray(float angle)
 {
-    t_xy player = g_game.player.pos;
+	t_xy player = g_game.player.pos;
 
-    t_xy vert_hit = find_vertical_hit(player, angle);
-    t_xy horiz_hit = find_horizontal_hit(player, angle);
+	t_xy vert_hit = find_vertical_hit(player, angle);
+	t_xy horiz_hit = find_horizontal_hit(player, angle);
 
-    if (distance(player, vert_hit) < distance(player, horiz_hit))
-        return (vert_hit);
-    return (horiz_hit);
+	if (distance(player, vert_hit) < distance(player, horiz_hit))
+		return (vert_hit);
+	return (horiz_hit);
 }
