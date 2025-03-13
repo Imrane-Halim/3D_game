52467d8a0079e984db9ec3d7eabe898ea829aa80
Imrane Halim <ihalim.professional@gmail.com>
2025-03-13 03:30:39 +0000

    changed number of rays casted

--- src/utils/initialization.c
+++ src/utils/initialization.c
@@ -25,7 +25,7 @@ void	init_game()
 	init_player();
 	// todo:
 	// g_game.scene = parse_map(av[1]);
-	g_game.n_rays = 320;
+	g_game.n_rays = WIDTH;
 	static char *map[] = 
 	{
 		"111111111111111111111111",
