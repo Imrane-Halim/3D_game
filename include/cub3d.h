a96c2c9f6e4e6e0ceabaabc9e3952910524eb974
Imrane Halim <ihalim@c3r6p8.1337.ma>
2025-03-15 21:05:47 +0000

    added floor and ceiling rendering

--- include/cub3d.h
+++ include/cub3d.h
@@ -18,7 +18,7 @@
 # define FORWARD	1
 # define BACKWARD  -1
 
-# define __DEBUG__	true
+# define __DEBUG__	false
 /*
 	possible implementation of s_scene;
 		typedef struct s_scene
--- src/rendering/plan_3d_view.c
+++ src/rendering/plan_3d_view.c
@@ -1,6 +1,13 @@
 #include "cub3d.h"
 
-void	draw_3D_view(void)
+void	draw_floor_ceiling()
 {
+	draw_square((t_xy){0, 0}, HEIGHT / 2, WIDTH, g_game.scene.ceiling_color);
+	draw_square((t_xy){0, HEIGHT / 2}, HEIGHT / 2, WIDTH, g_game.scene.floor_color);
+}
+
+inline void	draw_3D_view(void)
+{
+	draw_floor_ceiling();
 	
 }
