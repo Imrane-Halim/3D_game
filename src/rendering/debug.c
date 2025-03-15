16a332d2910d48d8432aabc8025c6897afde8051
Imrane Halim <ihalim.professional@gmail.com>
2025-03-15 14:21:02 +0000

    changed 2D map rending approach

--- src/rendering/debug.c
+++ src/rendering/debug.c
@@ -1,88 +1,123 @@
 #include "cub3d.h"
 
-// ------------------------- 2D helpers
-
-inline void	draw_map(void)
+t_xy	get_camera_offset()
 {
-	char	**map;
+	t_xy	offset;
+
+	offset.x = WIDTH / 2 - g_game.player.pos.x;
+	offset.y = HEIGHT / 2 - g_game.player.pos.y;
+
+	return (offset);
+}
+
+t_xy	applay_offset(t_xy pos, t_xy offset)
+{
+	return ((t_xy){pos.x + offset.x, pos.y + offset.y});
+}
+
+void	draw_background()
+{
+	int	i;
+
+	// background
+	draw_square((t_xy){0, 0}, HEIGHT, WIDTH, 0x262626);
+	
+	// horizontal grid
+	i = 0;
+	while (i < HEIGHT)
+	{
+		draw_line((t_xy){0, i}, (t_xy){WIDTH, i}, 0x333333);
+		i += TILESIZE;
+	}
+
+	// vertical grid
+	i = 0;
+	while (i < WIDTH)
+	{
+		draw_line((t_xy){i, 0}, (t_xy){i, HEIGHT}, 0x333333);
+		i += TILESIZE;
+	}
+}
+
+void	draw_map(t_xy offset)
+{
 	int		x;
 	int		y;
-
-	map = g_game.scene.map;
+	t_xy	pos;
+	
 	y = 0;
-	while (map[y])
+	while (g_game.scene.map[y])
 	{
 		x = 0;
-		while (map[y][x])
+		while (g_game.scene.map[y][x])
 		{
-			if (map[y][x] == '1')
-				draw_square((t_xy){x * TILESIZE, y * TILESIZE},
-					TILESIZE, TILESIZE, 0xffffff);
+			if (g_game.scene.map[y][x] == '1')
+			{
+				pos.y = y * TILESIZE;
+				pos.x = x * TILESIZE;
+				pos = applay_offset(pos, offset);
+				draw_square(pos, TILESIZE, TILESIZE, 0xffffff);
+			}
 			x++;
 		}
 		y++;
 	}
 }
 
-// note: player angle is radian
-// FOV in degrees
-inline void draw_fov()
+void	draw_player(t_xy offset)
 {
-	t_xy	ray;
-	
-	float	half = (FOV * PI / 180) / 2;
-	float	s_angle = g_game.player.angle - half;
-	float	e_angle = g_game.player.angle + half;
-	
-	float	step = (e_angle - s_angle) / g_game.n_rays;
+	t_xy	pos;
+
+	pos = applay_offset(g_game.player.pos, offset);
+	pos.x -= 8;
+	pos.y -= 8;
+	draw_square(pos, 16, 16, 0xe60b3e);
+}
+
+void	draw_fov(t_xy offset)
+{
+	t_xy	start;
+	t_xy	end;
+
+	float	step;
+	float	s_angle;
+	float	e_angle;
+
+	s_angle = g_game.player.angle - (FOV * PI / 180) / 2;
+	e_angle = g_game.player.angle + (FOV * PI / 180) / 2;
+	step = (FOV * PI / 180) / g_game.n_rays;
 	while (s_angle < e_angle)
 	{
-		ray = cast_ray(s_angle);
-		draw_line(g_game.player.pos, ray, 0x00ff00);
+		start = applay_offset(g_game.player.pos, offset);
+		end = applay_offset(cast_ray(s_angle), offset);
+		draw_line(start, end, 0x00ff00);
 		s_angle += step;
 	}
 }
 
-inline void	draw_player(void)
+void	draw_dir(t_xy offset)
 {
-	int	endx;
-	int	endy;
+	t_xy	start;
+	t_xy	end;
 
-	int startx;
-	int starty;
-
-	int	line_lenght;
+	float	line_lenght;
 
 	line_lenght = 100;
-	
-	startx = (int)g_game.player.pos.x;
-	starty = (int)g_game.player.pos.y;
-
-	// draw player square
-	draw_square((t_xy){startx - 8, starty - 8}, 16, 16, 0xf2a200);
-	
-	// draw fov
-	draw_fov();
-
-	// draw player looking direction
-
-	endx = startx + cos(g_game.player.angle) * line_lenght;
-	endy = starty + sin(g_game.player.angle) * line_lenght;
-
-	draw_line((t_xy){startx, starty}, (t_xy){endx, endy}, 0xff0000);
+	start = applay_offset(g_game.player.pos, offset);
+	end.x = g_game.player.pos.x + cos(g_game.player.angle) * line_lenght;
+	end.y = g_game.player.pos.y + sin(g_game.player.angle) * line_lenght;
+	end = applay_offset(end, offset);
+	draw_line(start, end, 0xe60b3e);
 }
-
-//--------------------------
 
 inline void	draw_2D_view(void)
 {
-	// gray background
-	draw_square((t_xy){0, 0}, HEIGHT, WIDTH, 0x2e2d2d);
+	t_xy	offset;
 
-	// draw map walls
-	draw_map();
-
-	// draw curr pos of player in map + looking direction
-	draw_player();
-
+	offset = get_camera_offset();
+	draw_background();
+	draw_map(offset);
+	draw_player(offset);
+	draw_fov(offset);
+	draw_dir(offset);
 }
