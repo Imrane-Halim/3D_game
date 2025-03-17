50c5808fb4612c6abe901d6cbbee680e3aeebec4
Imrane Halim <ihalim@c3r8p6.1337.ma>
2025-03-17 21:23:16 +0000

    added destroying images at exit

--- src/utils/close_game.c
+++ src/utils/close_game.c
@@ -1,9 +1,17 @@
 #include "cub3d.h"
 
+void	destroy_images()
+{
+	mlx_destroy_image(g_game.window.mlx, g_game.scene.textures.north.img);
+	mlx_destroy_image(g_game.window.mlx, g_game.scene.textures.south.img);
+	mlx_destroy_image(g_game.window.mlx, g_game.scene.textures.east.img);
+	mlx_destroy_image(g_game.window.mlx, g_game.scene.textures.west.img);
+}
+
 int		close_game(int code)
 {
 	// free_map();
-	// free_images();
+	destroy_images();
 	mlx_destroy_image(g_game.window.mlx, g_game.window.frame.img);
 	mlx_destroy_window(g_game.window.mlx, g_game.window.win);
 	mlx_destroy_display(g_game.window.mlx);
