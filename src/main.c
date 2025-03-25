fd14e0f98364c54f028b10ad18909620327377a8
Imrane-Halim <ihalim.professional@gmail.com>
2025-03-25 19:20:43 +0000

    fixed even more norm

--- src/main.c
+++ src/main.c
@@ -2,7 +2,7 @@
 
 t_game	g_game;
 
-int		game_loop()
+int	game_loop(void)
 {
 	g_game.timer++;
 	keyboard_input();
@@ -10,23 +10,26 @@ int		game_loop()
 	return (0);
 }
 
-void	start_game()
+void	start_game(void)
 {
-	// will be added later 
-	//mlx_mouse_hide(g_game.window.mlx, g_game.window.win);
-
+	// will be added later
+	// mlx_mouse_hide(g_game.window.mlx, g_game.window.win);
+	
 	mlx_hook(g_game.window.win, KeyPress, KeyPressMask, handle_press, NULL);
-	mlx_hook(g_game.window.win, KeyRelease, KeyReleaseMask, handle_release, NULL);
-	mlx_hook(g_game.window.win, DestroyNotify, StructureNotifyMask, close_game, NULL);
+	mlx_hook(g_game.window.win, KeyRelease, KeyReleaseMask,
+		handle_release, NULL);
+	mlx_hook(g_game.window.win, DestroyNotify, StructureNotifyMask,
+		close_game, NULL);
 	
 	// will be enabled before push
-	// mlx_hook(g_game.window.win, MotionNotify, PointerMotionMask, mouse_input, NULL);
+	// mlx_hook(g_game.window.win, MotionNotify, PointerMotionMask,
+	// 	mouse_input, NULL);
 
 	mlx_loop_hook(g_game.window.mlx, game_loop, NULL);
 	mlx_loop(g_game.window.mlx);
 }
 
-int main(int ac, char **av)
+int	main(int ac, char **av)
 {
 	if (ac != 2)
 	{
