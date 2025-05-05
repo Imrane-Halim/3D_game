29c99caf4e7ff9c4a643a10f17a4972ab1d4d6fd
Imrane Halim <ihalim.professional@gmail.com>
2025-05-05 09:52:12 +0100

    fixed a paths mem leak

--- src/utils/close_game.c
+++ src/utils/close_game.c
@@ -6,7 +6,7 @@
 /*   By: ihalim <ihalim@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/25 19:13:44 by imrane            #+#    #+#             */
-/*   Updated: 2025/04/22 15:32:07 by ihalim           ###   ########.fr       */
+/*   Updated: 2025/05/05 09:50:32 by ihalim           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -43,10 +43,21 @@ void	free_map(void)
 	free(g_game()->scene.map);
 }
 
+void	free_paths(void)
+{
+	free(g_game()->hand.path);
+	free(g_game()->scene.textures.north.path);
+	free(g_game()->scene.textures.south.path);
+	free(g_game()->scene.textures.west.path);
+	free(g_game()->scene.textures.east.path);
+	free(g_game()->scene.textures.door.path);
+}
+
 int	close_game(int code, char *message)
 {
 	if (code && message)
 		ft_putendl_fd(message, STDERR_FILENO);
+	free_paths();
 	free_map();
 	destroy_images();
 	mlx_destroy_image(g_game()->window.mlx, g_game()->window.frame.img);
