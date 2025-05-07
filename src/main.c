efb5111495e21faa8bda078be4864b07b38f479a
Imrane Halim <ihalim.professional@gmail.com>
2025-05-07 10:17:51 +0100

    fixed norm and added some comment

--- src/main.c
+++ src/main.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ihalim <ihalim@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/07 10:11:00 by ihalim            #+#    #+#             */
+/*   Updated: 2025/05/07 10:17:47 by ihalim           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "cub3d.h"
 
 inline t_game	*g_game(void)
@@ -23,21 +35,16 @@ int	game_loop(void)
 	return (0);
 }
 
+// mlx_hide_mouse()
 void	start_game(void)
 {
-	// will be added later
-	// mlx_mouse_hide(g_game()->window.mlx, g_game()->window.win);
-	
 	mlx_hook(g_game()->window.win, KeyPress, KeyPressMask, handle_press, NULL);
 	mlx_hook(g_game()->window.win, KeyRelease, KeyReleaseMask,
 		handle_release, NULL);
 	mlx_hook(g_game()->window.win, DestroyNotify, StructureNotifyMask,
 		close_game, NULL);
-	
-	// will be enabled before push
 	mlx_hook(g_game()->window.win, MotionNotify, PointerMotionMask,
-	 	mouse_input, NULL);
-
+		mouse_input, NULL);
 	mlx_loop_hook(g_game()->window.mlx, game_loop, NULL);
 	mlx_loop(g_game()->window.mlx);
 }
@@ -49,19 +56,13 @@ int	main(int ac, char **av)
 		printf("usage: ./cub3d <SCENE_PATH>");
 		return (EXIT_FAILURE);
 	}
-	//init_scene(av[1]);
-	t_scene scene = parse_map(av[1]);
-	//if (!scene)
-	//{
-	//	printf("invalid scene\n");
-	//	return (EXIT_FAILURE);
-	//}
-	//ft_memcpy(&g_game()->scene, scene, sizeof(t_scene));
-	g_game()->scene = scene;
-	g_game()->player.pos = (t_xy){100, 100};
+	g_game()->scene = parse_map(av[1]);
 	/*
-		u should find other method for init game because problem of leaks form ur code. 
-		i fixed door and color , now 
+		todo: player position
+		void	init_player(pos, angle);
+
+		ANGLE:	N S E W (PI / 2, PI + PI / 2, 0, PI)
+		POS:	pos better be pos + {32, 32};
 	*/
 	init_game();
 	start_game();
